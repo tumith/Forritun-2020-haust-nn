ed5032b9e9107b607a80132c7cd93d30e018050b
tumith <tumithorberg@gmail.com>
2020-10-08 12:29:28 +0000

    lok á sverk 3

--- docs/FORR3RR05DU/skilaverkefni3.py
+++ docs/FORR3RR05DU/skilaverkefni3.py
@@ -9,56 +9,60 @@ import random
 """
 
 #2
-  
-def bubble_sort(listt): 
-    for i, num in enumerate(listt): 
-        try: 
-            if listt[i+1] < num: 
-                listt[i] = listt[i+1] 
-                listt[i+1] = num 
-                bubble_sort(listt) 
-        except IndexError: 
-            pass
-    return listt 
-  # 1 2 3 4 5 6 7 8 9 10 12 13 14 15 16 17 18 19 20 21 22 23 25 26 27 28 29 30
-listt = [10, 5, 2, 4, 1, 3, 6, 9, 8 , 7, 20, 12, 14, 15, 13, 16, 18, 19, 17, 30, 25, 21, 23, 26, 22, 27, 29, 28, 24] 
-bubble_sort(listt) 
-  
-print("Sorted array:"); 
-for i in range(0, len(listt)): 
-    print(listt[i], end=' ') 
 
+# recursiv -----
+# def bubble_sort(listt): 
+#     for i, num in enumerate(listt): 
+#         try: 
+#             if listt[i+1] < num: 
+#                 listt[i] = listt[i+1] 
+#                 listt[i+1] = num 
+#                 bubble_sort(listt) 
+#         except IndexError: 
+#             return 0
+#     return listt 
+#   # 1 2 3 4 5 6 7 8 9 10 12 13 14 15 16 17 18 19 20 21 22 23 25 26 27 28 29 30
+# listt = [10, 5, 2, 4, 1, 3, 6, 9, 8 , 7, 20, 12, 14, 15, 13, 16, 18, 19, 17, 30, 25, 21, 23, 26, 22, 27, 29, 28, 24] 
+# bubble_sort(listt) 
+  
+# print("Sorted array:"); 
+# for i in range(0, len(listt)): 
+#     print(listt[i], end=' ') 
+# --------------------
 
 #endur taka til að læra betur
 
 startTime = time.time()
 
-list1 = []
+def bubble_sorta(arr):
+    n = len(arr) 
+    # Traverse through all array elements 
+    for i in range(n-1): 
+    # range(n) also work but outer loop will repeat one time more than needed. 
+        # Last i elements are already in place 
+        for j in range(0, n-i-1): 
+            # traverse the array from 0 to n-i-1 
+            # Swap if the element found is greater 
+            # than the next element 
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
 b = 0
+arr = []
 m = 1
-while(b < 1000000):
-    list1.append(m)
-    random.shuffle(list1)
+while(b < 1000):
+    arr.append(m)
+    random.shuffle(arr)
     m += 1
     b += 1
 
-def bubble_sorta(list1):
-    for e, tal in enumerate(list1):
-        try:
-            if list1[e+1] < tal:
-                list1[e] = list1[e+1]
-                list1[e+1] = tal
-                bubble_sorta(list1)
-        except IndexError:
-            pass
-    return list1
 
-
-bubble_sort(list1)
+print(arr,'\n')
+bubble_sorta(arr)
 
 print("Sorted array: ")
-for e in range(0, len(list1)):
-    print(list1[e], end=' ')
+for e in range(0, len(arr)):
+    print('\n',arr[e], end=' ')
 
 print('\n',time.time() - startTime)
 
@@ -139,5 +143,5 @@ def insertFall(listi, n):
 
 listi = [2,3,5,6,7,9,10]
 
-inp5 = int(input("#5 Slá inn orð "))
+inp5 = int(input("#5 Slá inn tölu "))
 print(insertFall(listi,inp5))
