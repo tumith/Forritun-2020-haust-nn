04c44646a5cc45638ae3ecb8be8bef41a1c54e85
tumith <tumithorberg@gmail.com>
2020-11-10 17:38:05 +0000

    verk

--- docs/FORR3RR05DU/verkefni6/verk6.py
+++ docs/FORR3RR05DU/verkefni6/verk6.py
@@ -76,4 +76,13 @@ class Tree:
 tree = Tree()
 tree.insert(5)
 tree.insert(3)
+tree.insert(6)
+tree.insert(1)
+tree.insert(2)
+tree.insert(7)
+tree.insert(10)
 tree.print_tree()
+
+print("tree height: " + str(tree.height()))
+
+print(tree.search(7))
