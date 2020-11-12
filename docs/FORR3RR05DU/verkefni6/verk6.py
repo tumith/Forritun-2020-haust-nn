25974824bdac8c4c8634afd4938078a1ef687e81
tumith <tumithorberg@gmail.com>
2020-11-12 23:16:09 +0000

    12.11.2020

--- docs/FORR3RR05DU/verkefni6/verk6.py
+++ docs/FORR3RR05DU/verkefni6/verk6.py
@@ -1,3 +1,6 @@
+# https://github.com/bfaure/Python_Data_Structures/blob/master/Binary_Search_Tree/main.py
+# https://www.youtube.com/watch?v=Zaf8EOVa72I&list=PLEJyjB1oGzx3iTZvOVedkT8nZ2cG105U7&index=6
+
 class Node:
     def __init__(self, value=None):
         self.value = value
@@ -38,19 +41,101 @@ class Node:
     def search(self,value,cur_node):
         if value == cur_node.value:
             return True
-        elif value < cur_node.value and cur_node.left_child != None:
-            return self.search(value,cur_node.left_child)
-        elif value > cur_node.value and cur_node.right_child != None:
-            return self.search(value,cur_node.right_child)
+        elif value < cur_node.value and cur_node.left != None:
+            return self.search(value,cur_node.left)
+        elif value > cur_node.value and cur_node.right != None:
+            return self.search(value,cur_node.right)
         return False
     
     def find(self,value,cur_node):
-        if value == cur_node:
+        if value==cur_node.value:
             return cur_node
-        elif value < cur_node.value and cur_node.left != None:
+        elif value<cur_node.value and cur_node.left!=None:
             return self.find(value,cur_node.left)
-        elif value > cur_node.value and cur_node.right != None:
+        elif value>cur_node.value and cur_node.right!=None:
             return self.find(value,cur_node.right)
+        
+    def delete_node(self,node):
+        # Vernda gegn því að reina að eyða node sem finnst ekki
+        if node == None or self.find(node.value) == None:
+            print("Node sem var verið að reina að eyða fannst ekki")
+            return None
+        
+        # skilar node með minsta value-ið
+        def min_value_node(n):
+            current = n
+            while current.left != None:
+                current = current.left
+            return current
+        
+        def num_children(n):
+            num_children = 0
+            if n.left != None:
+                num_children += 1
+            if n.right != None:
+                num_children += 1
+            return num_children
+        
+        # ná í foreldrið af node-inu sem á að vera eitt
+        node_parent = node.parent
+
+        # numer af child-inu af node-inu sem á að vera eitt
+        node_children = num_children(node)
+
+        # break operation into different cases based on the
+		# structure of the tree & node to be deleted
+
+        # CASE 1 (node has no children)
+        if node_children == 0:
+
+            # Added this if statement post-video, previously if you
+            # deleted the root node it would delete entire tree
+            if node_parent != None:
+                #remove reference to the node from the parent
+                if node_parent.left == node:
+                    node_parent.left = None
+                else:
+                    node_parent.right = None
+            else:
+                self.root = None
+        
+        # CASE 2 (node has a single child)
+        if node_children == 1:
+
+            # get the single child node
+            if node.left = None:
+                child = node.left
+            else:
+                child = node.right
+
+            # Adding this statement post-video, previously if you
+            # deleted the root node it would delete entire tree.
+            if node_parent != None:
+                # replace the node to be deleted with its child
+                if node_parent.left == node:
+                    node_parent.left = child
+                else:
+                    node_parent.right = child
+            else:
+                self.root = child
+            
+            # correct the parent pointer in node
+            child.parent = node_parent
+        
+        # CASE 3 (node has two children)
+        if node_children == 2:
+
+            # get the inorder successor of the deleted node
+            successor = min_value_node(node.right)
+
+            # copy the inorder successor's value to the node formerly
+            # holding the value we wished to delete
+            node.value = successor.value
+
+            # delete the inorder successor now that it's value was
+            # copied into the other node
+            self.delete_node(successor)
+
 
 
 class Tree:
@@ -83,7 +168,10 @@ class Tree:
         if self.root != None:
             return self.root.find(value,self.root)
         else:
-            return None    
+            return None
+        
+    def delete_value(self, value):
+        return self.root.delete_node(self.find(value))
 
 
 
@@ -100,5 +188,3 @@ tree.print_tree()
 print("tree height: " + str(tree.height()))
 
 print(tree.search(7))
-
-print(tree.find(3))
